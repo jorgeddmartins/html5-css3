fddeb18f9a47687c09132e721f45f3c4024d8278
Jorge DD Martins <jorgedd.martins@hotmail.com>
2014-05-14 10:04:47 +0100

    html5 and css3 codes
    Two projects. Ladyx and Fashionable

---
+++ ladyx/js/toggle.js
@@ -0,0 +1,11 @@
+
+// Menu inside Nav
+$(document).ready(function(){
+            $('#menu a').click(function(e){
+                e.preventDefault();
+                $(this).closest('li').find('>ul').slideToggle();
+            });
+        });
+
+
+
